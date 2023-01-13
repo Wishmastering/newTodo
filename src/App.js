9d2accdc9013f58fa32e777cbbdbb177cab2335e
Wishmastering <charsdb@gmail.com>
2023-01-12 22:36:00 -0600

    Todo List Terminado sin estilo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import "../src/style.css"
+import Draggable from 'react-draggable';
+
 
 function App() {
+  let [list, setList] = useState([])
+  let [todo, setTodo] = useState("")
+
+  function submit(){
+    if(todo === ""){
+      alert("Maldito Imbecil Escribe No Seas Perezoso")
+    }else{
+      setList([...list, todo])   
+      setTodo("")
+    }
+  }
+
+  function handleKey(e){
+    if(e.key === 'Enter'){
+      if(todo === ""){
+        alert("Maldito Imbecil Escribe No Seas Perezoso")
+      }else{
+        setList([...list, todo])   
+        setTodo("")
+      }
+    }
+  }
+
+  function eliminar(num){
+    let pizzerolas = list.filter
+    
+    ((item,i)=> {if(num !== i){
+      return item
+    }})
+    setList(pizzerolas)
+
+  }
+    
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="">
+          To Do List with React and useState
+      </div>
+      <div>
+        <input placeholder="Memo esta muy fumarola"  value={todo} onKeyDown={handleKey} onChange={(e)=>setTodo(e.target.value)}/>
+      <div>
+          <button onClick={submit} > Submit </button>
+        </div>
+        <div>
+          {list.map((item,index)=>
+            <li key={index}>{item} <span onClick={()=>eliminar(index)}> X </span></li>
+          )}
+        </div>
+      </div>
+    </>
   );
 }
 
 export default App;
+
+
